39337ce62a1a7768cd58bcffa655593fb81578ca
Wilson Guo <wguo91@gmail.com>
2018-12-02 23:45:48 -0800

    Modify server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
+app.set("port", process.env.PORT || 3000);
 
 // serve static files
 app.use(express.static(__dirname));
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(port, () => {
-  console.log("Application listening on port " + port);
+app.listen(app.get("port"), () => {
+  console.log("Application listening on port " + app.get("port"));
 });
