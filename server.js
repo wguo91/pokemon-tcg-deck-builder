1c41c48288a93ee3b3e85771cb9d4dd1cb87cbed
Wilson Guo <wguo91@gmail.com>
2018-12-05 00:23:58 -0800

    Reorganize and added localstorage support to keep track of cards on page refresh

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 app.set("port", process.env.PORT || 3000);
 
 // serve static files
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(app.get("port"), () => {
